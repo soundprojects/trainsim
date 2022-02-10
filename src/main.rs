902001f4ae54ab28a549b10820d6c32c35a2f642
wouter <w.janmaat@me.com>
2022-02-10 10:14:56 +0100

    Changed job to use Interval for 5 seconds

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use futures::executor::block_on;
 //use async_timer::Interval;
+
+
 ///Increment Timer
 ///
 ///We increment our counter
@@ -11,11 +13,20 @@ async fn increment_timer(counter: &mut i32){
 
 ///Runs our timer and starts jobs
 ///
-///This sets up a dependence on the future provided by increment_timer
-///For now we just for increment_timer to return
+///This sets up a dependency on the future provided by increment_timer
+///For now we just run increment_timer for 5 seconds using the Interval
 ///
 async fn run_timer(counter: &mut i32){
-increment_timer(counter).await;
+    let mut times: i32 = 0;
+    let mut interval = async_timer::Interval::platform_new(core::time::Duration::from_secs(1));
+
+//for now we wait for 5 seconds to complete
+    while times < 5
+    {
+        increment_timer(counter).await;
+        interval.wait().await;
+        times += 1;
+    }
 }
 
 fn main() {
