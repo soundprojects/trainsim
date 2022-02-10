efe56e03c16c0760c16cebaf2676aec401352831
wouter <w.janmaat@me.com>
2022-02-10 10:08:27 +0100

    Add dependencies for async await, add two async functions to increment counter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use futures::executor::block_on;
+//use async_timer::Interval;
+///Increment Timer
+///
+///We increment our counter
+///For now we just increment counter by 1
+///
+async fn increment_timer(counter: &mut i32){
+    *counter += 1;
+}
+
+///Runs our timer and starts jobs
+///
+///This sets up a dependence on the future provided by increment_timer
+///For now we just for increment_timer to return
+///
+async fn run_timer(counter: &mut i32){
+increment_timer(counter).await;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut counter: i32 = 0;
+
+    block_on(run_timer(&mut counter));
+
+    println!("Count is: {}", counter);
 }
