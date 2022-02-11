791025aa66c13f2fd155050183789cd9b117d572
Ruurd Adema <ruurd@owllab.nl>
2022-02-11 12:09:08 +0100

    Simplify timer logic (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,48 @@
-use std::sync::atomic::{AtomicUsize, Ordering};
 use async_timer::Interval;
 
-///Increment Timer
+/// Increment Timer
 ///
-///We increment our counter
-///For now we just increment counter by 1
+/// We increment our counter
+/// For now we just increment counter by 1
 ///
-fn increment_timer(counter: std::sync::Arc<AtomicUsize>) {
-    let prev = counter.fetch_add(1, Ordering::SeqCst);
-    println!("Counter is {}", prev + 1);
+fn increment_timer(counter: &mut usize) {
+    *counter += 1;
+    println!("Counter is {}", counter);
 }
 
-///Runs our timer and starts jobs
+/// Runs our timer and starts jobs
 ///
-///This sets up a dependency on the future provided by increment_timer
-///For now we just run increment_timer for 5 seconds using the Interval
+/// This sets up a dependency on the future provided by increment_timer
+/// For now we just run increment_timer for 5 seconds using the Interval
 ///
-async fn run_timer(counter: std::sync::Arc<AtomicUsize>) {
+async fn run_timer() -> usize {
+    let mut counter: usize = 0;
     let mut times: i32 = 0;
     let mut interval = Interval::platform_new(core::time::Duration::from_secs(1));
 
-//for now we wait for 10 seconds to complete
+    // For now we wait for 10 seconds to complete/
     while times < 10
     {
-        increment_timer(counter.clone());
+        increment_timer(&mut counter);
         interval.wait().await;
         times += 1;
     }
+
+    counter
 }
 
-//Tokio::main macro translates the main function back to a non-async function
+// Tokio::main macro translates the main function back to a non-async function
 // .await calls are transformed to block_on to make for easy coding
 #[tokio::main]
 async fn main() {
-    let counter = std::sync::Arc::new(AtomicUsize::new(0));
+    let task = tokio::spawn(run_timer());
 
-    {
-        let counter = counter.clone();
-        let task = tokio::spawn(async move {
-            run_timer(counter).await;
-        });
+    // Block on our task
+    MainWindow::new().run();
 
-        //block on our task
-        MainWindow::new().run();
+    let counter = task.await.unwrap();
 
-        let _result = task.await.unwrap();
-    }
-
-    println!("Count is: {}", counter.load(Ordering::SeqCst));
+    println!("Count is: {}", counter);
 }
 
 slint::slint! {
