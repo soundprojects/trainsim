1be11a0c7bfaa4aacaaff9d44effcb83a08b12b9
wouter <w.janmaat@me.com>
2022-02-11 10:48:38 +0100

    Added simple slint window, moved counter to end of program

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use async_timer::Interval;
 
-
 ///Increment Timer
 ///
 ///We increment our counter
@@ -8,6 +7,7 @@ use async_timer::Interval;
 ///
 async fn increment_timer(counter: &mut i32){
     *counter += 1;
+    println!("Counter is {}", counter);
 }
 
 ///Runs our timer and starts jobs
@@ -19,8 +19,8 @@ async fn run_timer(counter: &mut i32){
     let mut times: i32 = 0;
     let mut interval = Interval::platform_new(core::time::Duration::from_secs(1));
 
-//for now we wait for 5 seconds to complete
-    while times < 5
+//for now we wait for 10 seconds to complete
+    while times < 10
     {
 
         increment_timer(counter).await;
@@ -29,18 +29,33 @@ async fn run_timer(counter: &mut i32){
     }
 }
 
+
 //Tokio::main macro translates the main function back to a non-async function
 // .await calls are transformed to block_on to make for easy coding
 #[tokio::main]
 async fn main() {
+
     let mut counter: i32 = 0;
 
     let task = tokio::spawn(async move {
         run_timer(&mut counter).await;
-        println!("Count is: {}", counter);
+        
     });
 
     //block on our task
-    let _result = task.await.unwrap(); 
+    MainWindow::new().run();
 
+    let _result = task.await.unwrap(); 
+    println!("Count is: {}", counter);
+
+}
+
+
+slint::slint! {
+    MainWindow := Window {
+        Text {
+            text: "hello world";
+            color: green;
+        }
+    }
 }
