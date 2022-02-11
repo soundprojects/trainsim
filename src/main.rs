aa8978861baec34b933a677fafd848b2dfc591cb
Ruurd Adema <ruurd@owllab.nl>
2022-02-11 11:12:50 +0100

    Make counter an Arc<AtomicUsize> (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
 use async_timer::Interval;
 
 ///Increment Timer
@@ -5,9 +6,9 @@ use async_timer::Interval;
 ///We increment our counter
 ///For now we just increment counter by 1
 ///
-async fn increment_timer(counter: &mut i32){
-    *counter += 1;
-    println!("Counter is {}", counter);
+async fn increment_timer(counter: std::sync::Arc<AtomicUsize>) {
+    let prev = counter.fetch_add(1, Ordering::SeqCst);
+    println!("Counter is {}", prev + 1);
 }
 
 ///Runs our timer and starts jobs
@@ -15,15 +16,14 @@ async fn increment_timer(counter: &mut i32){
 ///This sets up a dependency on the future provided by increment_timer
 ///For now we just run increment_timer for 5 seconds using the Interval
 ///
-async fn run_timer(counter: &mut i32){
+async fn run_timer(counter: std::sync::Arc<AtomicUsize>) {
     let mut times: i32 = 0;
     let mut interval = Interval::platform_new(core::time::Duration::from_secs(1));
 
 //for now we wait for 10 seconds to complete
     while times < 10
     {
-
-        increment_timer(counter).await;
+        increment_timer(counter.clone()).await;
         interval.wait().await;
         times += 1;
     }
@@ -34,22 +34,22 @@ async fn run_timer(counter: &mut i32){
 // .await calls are transformed to block_on to make for easy coding
 #[tokio::main]
 async fn main() {
+    let counter = std::sync::Arc::new(AtomicUsize::new(0));
 
-    let mut counter: i32 = 0;
+    {
+        let counter = counter.clone();
+        let task = tokio::spawn(async move {
+            run_timer(counter).await;
+        });
 
-    let task = tokio::spawn(async move {
-        run_timer(&mut counter).await;
-        
-    });
+        //block on our task
+        MainWindow::new().run();
 
-    //block on our task
-    MainWindow::new().run();
+        let _result = task.await.unwrap();
+    }
 
-    let _result = task.await.unwrap(); 
-    println!("Count is: {}", counter);
-
+    println!("Count is: {}", counter.load(Ordering::SeqCst));
 }
-
 
 slint::slint! {
     MainWindow := Window {
