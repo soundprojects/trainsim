8fc93400d23ac01a91efb577317a46e39d7fe336
wouter <w.janmaat@me.com>
2022-02-11 13:48:49 +0100

    Made .slint file external, added working counter in the ui. Cleanup.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
+use slint::SharedString;
 use async_timer::Interval;
 
-///Increment Timer
-///
-///We increment our counter
-///For now we just increment counter by 1
-///
-async fn increment_timer(counter: &mut i32){
-    *counter += 1;
-    println!("Counter is {}", counter);
-}
 
 ///Runs our timer and starts jobs
 ///
-///This sets up a dependency on the future provided by increment_timer
-///For now we just run increment_timer for 5 seconds using the Interval
+///This runs a timer at a 1s interval and updates the UI
+///For now we just run this for 10 seconds
 ///
-async fn run_timer(counter: &mut i32){
+async fn run_timer(handle: slint::Weak<MainWindow>){
+    
+    let mut counter: i32 = 0;
     let mut times: i32 = 0;
     let mut interval = Interval::platform_new(core::time::Duration::from_secs(1));
 
@@ -23,7 +17,14 @@ async fn run_timer(counter: &mut i32){
     while times < 10
     {
 
-        increment_timer(counter).await;
+        //increment
+        counter += 1;
+        println!("Counter is {}", counter);
+
+        //update our counter in the UI
+        handle.clone().upgrade_in_event_loop(move |h| h.set_counter(SharedString::from(counter.to_string())));
+
+        //wait a second
         interval.wait().await;
         times += 1;
     }
@@ -32,30 +33,31 @@ async fn run_timer(counter: &mut i32){
 
 //Tokio::main macro translates the main function back to a non-async function
 // .await calls are transformed to block_on to make for easy coding
+//Slint include modules allows use to import our external slint files using build.rs
+slint::include_modules!();
 #[tokio::main]
 async fn main() {
 
-    let mut counter: i32 = 0;
+    //start our Slint Window
+    let window = MainWindow::new();
 
+    //get reference to our window that we can send across threads
+    let handle = window.as_weak();
+
+    //start timer task and pass our window
     let task = tokio::spawn(async move {
-        run_timer(&mut counter).await;
+        run_timer(handle).await;
         
     });
 
-    //block on our task
-    MainWindow::new().run();
 
+    //run window
+    window.run();
+
+    //block on our timer task
     let _result = task.await.unwrap(); 
-    println!("Count is: {}", counter);
 
+    //if we got here, timer is done and window is closed
+    println!("Program is quitting");
 }
 
-
-slint::slint! {
-    MainWindow := Window {
-        Text {
-            text: "hello world";
-            color: green;
-        }
-    }
-}
