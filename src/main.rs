bc23c0f26977e57a9c50663ed9f8e3b668e9b238
Ruurd Adema <ruurd@owllab.nl>
2022-02-15 14:30:07 +0100

    Playground 15 jan

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use crate::worker::worker_loop;
 use slint::SharedString;
 
 mod worker;
-
 
 //Tokio::main macro translates the main function back to a non-async function
 // .await calls are transformed to block_on to make for easy coding
@@ -9,35 +9,41 @@ mod worker;
 slint::include_modules!();
 #[tokio::main]
 async fn main() {
-
     //start our Slint Window
     let window = MainWindow::new();
 
     //start worker task and pass our window handle
-    let worker = worker::Worker::new(&window);
+    // let worker = worker::Worker::new(&window);
+
+    let (channel, r) = tokio::sync::mpsc::unbounded_channel();
 
     //assign callbacks
+    let clone = channel.clone();
     window.on_reset({
-        let channel = worker.channel.clone();
+        // let channel = worker.channel.clone();
         move || {
-        channel.send(worker::WorkerMessage::Reset).unwrap();
+            clone.send(worker::WorkerMessage::Reset).unwrap();
         }
     });
 
     window.on_set_counter({
-        let channel = worker.channel.clone();
+        // let channel = worker.channel.clone();
         move |number| {
-        channel.send(worker::WorkerMessage::Counter(number)).unwrap();
+            channel
+                .send(worker::WorkerMessage::Counter(number))
+                .unwrap();
         }
+    });
+
+    let handle_weak = window.as_weak();
+
+    let mut worker = tokio::spawn(async move {
+        worker_loop(r, handle_weak).await;
     });
 
     //run window
     window.run();
 
-
-    //if we got here, worker window is closed, so we join worker thread
-    println!("Program is quitting");
-
-    worker.join().unwrap();
+    // If we got here, worker window is closed, so we join worker thread
+    println!("Clean exit");
 }
-
--- src/worker.rs
+++ src/worker.rs
@@ -1,36 +1,32 @@
-
-use futures::future::{FutureExt};
-use tokio::{select, time::{interval}};
+use super::*;
+use futures::future::FutureExt;
 use std::time::Duration;
 use tokio::sync::mpsc::UnboundedReceiver;
 use tokio::sync::mpsc::UnboundedSender;
-use super::*;
-
+use tokio::{select, time::interval};
 
 ///Worker Message Enumerator
 ///For now just contains Quit and Counter for updating our counter from the UI
 #[derive(Debug)]
-pub enum WorkerMessage{
-    Quit, Counter(i32), Reset,
+pub enum WorkerMessage {
+    Quit,
+    Counter(i32),
+    Reset,
 }
 
 ///Worker struct definition
 ///Channel and thread defined
-pub struct Worker{
-
-pub channel: UnboundedSender<WorkerMessage>,
-worker_thread: std::thread::JoinHandle<()>
+pub struct Worker {
+    pub channel: UnboundedSender<WorkerMessage>,
+    worker_thread: std::thread::JoinHandle<()>,
 }
 
 ///Worker implementation
 ///implementation creates our worker thread and a channel for communication
-impl Worker{
-
-    pub fn new(handle: &MainWindow) -> Self{
-
+impl Worker {
+    pub fn new(handle: &MainWindow) -> Self {
         let (channel, r) = tokio::sync::mpsc::unbounded_channel();
         let worker_thread = std::thread::spawn({
-
             let handle_weak = handle.as_weak();
 
             move || {
@@ -41,76 +37,76 @@ impl Worker{
             }
         });
 
-        Self{
+        Self {
             channel,
-            worker_thread
+            worker_thread,
         }
-
     }
 
     ///Join is run when the window is closed. We pass a quit message to stop our worker loop from running
-    pub fn join(self) -> std::thread::Result<()>{
+    pub fn join(self) -> std::thread::Result<()> {
         let _ = self.channel.send(WorkerMessage::Quit);
         self.worker_thread.join()
     }
-
 }
 
+struct State {
+    counter: i32,
+}
 
 ///Worker loop keeps running our defined tasks until the program is quit
 ///it allows for the UI to be updated through our weak handle
 ///sender and receivers are used for callback communication and other signals
-async fn worker_loop(mut r: UnboundedReceiver<WorkerMessage>,
-    handle: slint::Weak<MainWindow>)
--> tokio::io::Result<()>{
-
-    let mut counter: i32 = 0;
+pub async fn worker_loop(
+    mut r: UnboundedReceiver<WorkerMessage>,
+    handle: slint::Weak<MainWindow>,
+) -> tokio::io::Result<()> {
+    let mut state = State { counter: 0};
     let mut interval = interval(Duration::from_secs(1));
 
     //This loop runs tick and then checks for messages, if there is one, run actions
-    loop{
-        let m = select!{
-
+    loop {
+        let m = select! {
 
             _ = interval.tick() => {
                 //Increment counter and update UI until 10
-                if counter < 10 {
-                counter += 1;
-                handle.clone().upgrade_in_event_loop(move |h|
-                h.set_counter(SharedString::from(counter.to_string())));}
+                if state.counter < 10 {
+                state.counter += 1;
+                handle.upgrade_in_event_loop(move |h|
+                h.set_counter(SharedString::from(state.counter.to_string())));}
+                println!("Tick");
+
                 continue;
             }
 
             m = r.recv().fuse() => {
                 match m {
                     None => return Ok(()),
-                    Some(m) => m,
+                    Some(m) => {
+                        println!("MSG");
+                        m
+                    },
                 }
             }
-
         };
 
-
-    //We got a message from our receiver
-    match m{
+        //We got a message from our receiver
+        match m {
             WorkerMessage::Quit => return Ok(()),
 
             WorkerMessage::Counter(number) => {
-                counter = number;
-                handle.clone()
-                .upgrade_in_event_loop(move |h|
-                h.set_counter(SharedString::from(counter.to_string()))
-                );
-            },
+                state.counter = number;
+                handle.clone().upgrade_in_event_loop(move |h| {
+                    h.set_counter(SharedString::from(state.counter.to_string()))
+                });
+            }
 
             WorkerMessage::Reset => {
-                counter = 0;
-                handle.clone()
-                .upgrade_in_event_loop(move |h|
-                h.set_counter(SharedString::from(counter.to_string()))
-                );
-            },
+                state.counter = 0;
+                handle.clone().upgrade_in_event_loop(move |h| {
+                    h.set_counter(SharedString::from(state.counter.to_string()))
+                });
+            }
         }
     }
 }
-
