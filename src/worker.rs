1dc3cfba9b49b25424c8d42d8381110400dff9cd
wouter <w.janmaat@me.com>
2022-02-15 16:33:16 +0100

    Decoupled UI from worker, removed struct, add channel for updating ui from loop

--- src/worker.rs
+++ src/worker.rs
@@ -4,7 +4,6 @@ use tokio::{select, time::{interval}};
 use std::time::Duration;
 use tokio::sync::mpsc::UnboundedReceiver;
 use tokio::sync::mpsc::UnboundedSender;
-use super::*;
 
 
 ///Worker Message Enumerator
@@ -14,58 +13,17 @@ pub enum WorkerMessage{
     Quit, Counter(i32), Reset,
 }
 
-///Worker struct definition
-///Channel and thread defined
-pub struct Worker{
-
-pub channel: UnboundedSender<WorkerMessage>,
-worker_thread: std::thread::JoinHandle<()>
-}
-
-///Worker implementation
-///implementation creates our worker thread and a channel for communication
-impl Worker{
-
-    pub fn new(handle: &MainWindow) -> Self{
-
-        let (channel, r) = tokio::sync::mpsc::unbounded_channel();
-        let worker_thread = std::thread::spawn({
-
-            let handle_weak = handle.as_weak();
-
-            move || {
-                tokio::runtime::Runtime::new()
-                    .unwrap()
-                    .block_on(worker_loop(r, handle_weak))
-                    .unwrap()
-            }
-        });
-
-        Self{
-            channel,
-            worker_thread
-        }
-
-    }
-
-    ///Join is run when the window is closed. We pass a quit message to stop our worker loop from running
-    pub fn join(self) -> std::thread::Result<()>{
-        let _ = self.channel.send(WorkerMessage::Quit);
-        self.worker_thread.join()
-    }
-
-}
-
-
 ///Worker loop keeps running our defined tasks until the program is quit
 ///it allows for the UI to be updated through our weak handle
 ///sender and receivers are used for callback communication and other signals
-async fn worker_loop(mut r: UnboundedReceiver<WorkerMessage>,
-    handle: slint::Weak<MainWindow>)
+pub async fn worker_loop(mut r: UnboundedReceiver<WorkerMessage>,
+    t: UnboundedSender<i32>)
 -> tokio::io::Result<()>{
 
     let mut counter: i32 = 0;
     let mut interval = interval(Duration::from_secs(1));
+
+    let channel = t.clone();
 
     //This loop runs tick and then checks for messages, if there is one, run actions
     loop{
@@ -76,8 +34,8 @@ async fn worker_loop(mut r: UnboundedReceiver<WorkerMessage>,
                 //Increment counter and update UI until 10
                 if counter < 10 {
                 counter += 1;
-                handle.clone().upgrade_in_event_loop(move |h|
-                h.set_counter(SharedString::from(counter.to_string())));}
+                println!("tick");
+                channel.send(counter).unwrap();}
                 continue;
             }
 
@@ -90,25 +48,18 @@ async fn worker_loop(mut r: UnboundedReceiver<WorkerMessage>,
 
         };
 
-
     //We got a message from our receiver
     match m{
             WorkerMessage::Quit => return Ok(()),
 
             WorkerMessage::Counter(number) => {
                 counter = number;
-                handle.clone()
-                .upgrade_in_event_loop(move |h|
-                h.set_counter(SharedString::from(counter.to_string()))
-                );
+                channel.send(counter).unwrap();
             },
 
             WorkerMessage::Reset => {
                 counter = 0;
-                handle.clone()
-                .upgrade_in_event_loop(move |h|
-                h.set_counter(SharedString::from(counter.to_string()))
-                );
+                channel.send(counter).unwrap();
             },
         }
     }
